c9c04968fd69e345977641550053f8d69d1ec8b2
Juan Gomez Fernandez <juan.gomez@orbitgroup.ca>
2021-03-02 21:10:27 -0500

    oop testing

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,16 +8,24 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Juan Gomez");
+            book.GradeAdded += OnGradeAdded;
+
             book.AddGrade(54.23);
             book.AddGrade(74.23);
             book.AddGrade(94.23);
 
             var result = book.GetStatistics();
 
+            Console.WriteLine(Book.test1); // se llama directamente desde la clase
             Console.WriteLine($"Lowest {result.Low}");
             Console.WriteLine($"Highest {result.High}");
             Console.WriteLine($"Average {result.Average:N1}");
             Console.WriteLine($"Letter {result.Letter}");
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
         }
     }
 
